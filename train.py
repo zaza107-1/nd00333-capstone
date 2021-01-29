2026b82886a9d482d405390000f35746c986feaf
zaza107-1 <61068402+zaza107-1@users.noreply.github.com>
2021-01-29 16:26:29 -0300

    Update train.py

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.metrics import mean_squared_error
 import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
+#from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
